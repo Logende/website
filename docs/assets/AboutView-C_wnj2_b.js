6e4dfdcc68fecdb0713dacd37b9c10fdd87cc51a
Logende <33369081+Logende@users.noreply.github.com>
2026-08-14 08:52:56 +0000

    Deploying to gh-pages from @ Logende/website@d7b9310cd694e7abe4ec4744d719a05ad36d4091 🚀

--- docs/assets/AboutView-C_wnj2_b.js
+++
@@ -1,4 +0,0 @@
-import { _ as _sfc_main } from "./index-BY55MHcy.js";
-export {
-  _sfc_main as default
-};
--- docs/assets/index-ObSe3_yz.js
+++ docs/assets/index-ObSe3_yz.js
@@ -1,4 +1,4 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/VeiledKingdomsView-BqGigh7z.js","assets/VeiledKingdomsView-CteRSTeH.css"])))=>i.map(i=>d[i]);
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/VeiledKingdomsView-CfPO7Roh.js","assets/VeiledKingdomsView-CteRSTeH.css"])))=>i.map(i=>d[i]);
 var __defProp = Object.defineProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
 var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
@@ -16858,7 +16858,9 @@ const projects = [
       end: "2016-09-11"
     },
     project_page: "https://dev.bukkit.org/projects/blockcommand",
-    video: "https://www.youtube.com/watch?v=Rl3Lw9ppvWw",
+    videos: [
+      "Rl3Lw9ppvWw"
+    ],
     relations: [
       {
         label: "relatedTo",
@@ -75906,7 +75908,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => __vitePreload(() => import("./AboutView-C_wnj2_b.js"), true ? [] : void 0)
+      component: () => __vitePreload(() => import("./AboutView-CQ7-xmZE.js"), true ? [] : void 0)
     },
     {
       path: "/projects",
@@ -75935,7 +75937,7 @@ const router = createRouter({
     {
       path: "/veiled-kingdoms",
       name: "veiled-kingdoms",
-      component: () => __vitePreload(() => import("./VeiledKingdomsView-BqGigh7z.js"), true ? __vite__mapDeps([0,1]) : void 0)
+      component: () => __vitePreload(() => import("./VeiledKingdomsView-CfPO7Roh.js"), true ? __vite__mapDeps([0,1]) : void 0)
     }
   ]
 });
